80775a8c097811f4bc9e6a60f2e3566c857fe8f9
aviroop1 <30727331+aviroop1@users.noreply.github.com>
2018-03-27 00:48:40 -0700

    Update and rename PortoSeguro.R to PortoSeguro Driver Kaggle.R

--- PortoSeguro Driver Kaggle.R
+++ PortoSeguro Driver Kaggle.R
@@ -1,16 +1,23 @@
+#Read Train and Test files
 Train = read.csv('Train.csv')
 Test = read.csv('Test.csv')
+
+#Understand the data 
 length(table(Train$id))
 sapply(Train, function(x){class(x)})
+
+#Plot and understand the correlation
 library(corrplot)
 corrplot(cor(Train))
 Correlation = data.frame(cor(Train))
 
+#Load regular expression library
 library(regex)
 
+#Initialize count variable
 count = 0
 
-
+#Loading all correlation values into a single array
 a = character(0)
 for (i in (1:59))
 {
@@ -20,27 +27,35 @@ for (i in (1:59))
   }
 }
 
+#Convert 'a' to numeric array
 a = as.numeric(a)
 
+#Arrange the correlations in descending order
 a = a[order(a, decreasing = T)]
-tail(a)
 
+#Remove all correlations with values as 1, since those indicate variable correlated with itself
 a = a[a != 1]
 
+#Check distribution of the correlation values
 quantile(a)
 
+#Define b and c as numeric array
 b = numeric(0); c=numeric(0)
 
+#Take all the correlation values greater than 0.5 and load into b and c
 for (i in (1:3422))
 {
 ifelse((a[i]>=0.5 || a[i]<= -0.5), b <- append(b, a[i]), c <- append(c, a[i]))
 }
 
+#Convert the values to 2 decimal places and returns a matrix
 m = data.frame(sapply(Correlation, function(x){as.numeric(format(x, digits = 2))}))
 
+#Define a new data frame DF
 DF = data.frame(RowName = character(), ColName = character(), Correlation = numeric(), stringsAsFactors = F)
 i=0; j=0; k=0
 
+#The DF should consist of the critical correlation values and the variable names having those strong correlation values
 for (i in (1:59))
 {
   for (j in (1:59))
@@ -80,6 +95,7 @@ strsplit(a[2],"")[2]
 strsplit(a[1:5], "")[[1]][1]
 which(strsplit(a[1],"")[[1]] == "e")
 
+#Load the required libraries for running the ML algorithms
 library(xgboost)
 library(caTools)
 
@@ -96,16 +112,25 @@ names = dimnames(Training)[[2]]
 importance.matrix = xgb.importance(names, model = xgboost)
 xgb.plot.importance(importance.matrix)
 
+#Predict using xgboost model
 Predict = predict(xgboost, newdata = as.matrix(Testing))
 
+#Confusion Matrix
 table(Testing$target, Predict>0.5)
 
+#Tell R to not use the scientific notation
 options(scipen = 3)
 
+#Combining the prediction columns with ID column of Test dataset
 TestPrediction = data.frame(cbind(Test$id, Predict))
+
+#Change column names to 'id' and 'target'
 colnames(TestPrediction) = c("id", "target")
+
+#Write the Test Prediction values to a csv file
 write.csv(TestPrediction, "TestPredictionPortoSeguro.csv")
 
+#Load the DF Matrix value containing highest correlation values, to another data frame called 'HighestCorrDF'
 HighestCorrDF = DF
 DF = NULL
 
@@ -117,16 +142,14 @@ Testing = Testing[,-a]
 LogReg = glm(target ~., data = Training[,-1], family = binomial)
 summary(LogReg)
 
+#Predict using Logistic Regression model
 PredictLogReg = predict(LogReg, newdata = Testing, type = "response")
 
+#Understand the prediction
 quantile(PredictLogReg)
 sum(Testing$target==0)/nrow(Testing)
 mean(PredictLogReg)
 
+#Confusion Matrix
 cf = table(Testing$target, PredictLogReg>0.0364173)
 sum(diag(cf))/sum(cf)
-PredictLogReg[1:10]
-
-grep("calc",names(Training), value = T )
-
-summary(LogReg)
